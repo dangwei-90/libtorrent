0a594332766e0a027e604ecfe772f4fa7c048b33
dangwei <dangwei@360.cn>
2021-01-05 14:54:45 +0800

    [tracker] add resp.complete.
    Change-Id: I84e9b21e0f22a91b417f2898ecc097fe41561846

--- src/http_tracker_connection.cpp
+++ src/http_tracker_connection.cpp
@@ -474,7 +474,9 @@ namespace libtorrent {
 #endif
 				sprintf(curr_path, "%s\\tracker_path\\tracker_tmp", curr_path);
 
-				std::string write_data = tracker_req().url + "*" + aux::to_hex(tracker_req().info_hash) + "*" + std::to_string(num_peers) + "#";
+				std::string write_data = tracker_req().url + "*" + aux::to_hex(tracker_req().info_hash) + "*" 
+					+ std::to_string(num_peers) + "*" + std::to_string(resp.complete)
+					+ "#";
 				std::fstream sfile(curr_path, std::ios::app | std::ios::out | std::ios_base::binary);
 				sfile.write(write_data.c_str(), write_data.size());
 				sfile.close();
